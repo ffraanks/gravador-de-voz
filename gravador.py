4903d08380b47ae39f162114b9da7fbad500c88a
Franklin Souza <83979476+ffraanks@users.noreply.github.com>
2021-07-14 21:41:16 -0300

    Create gravador.py

---
+++ gravador.py
@@ -0,0 +1,20 @@
+#!/bin/python3
+
+# pip install --user sounddevice
+# pip install --user scipy
+
+import sounddevice
+from scipy.io.wavfile import write
+
+# Tempo de gravação
+fs = 44100
+second = int(input('Quantos segundos deseja gravar?:\n\n '))
+print('\nGravando...')
+
+# Captura a gravação
+record_voice=sounddevice.rec(int(second * fs) , samplerate=fs , channels=2)
+sounddevice.wait()
+
+# Salva arquivo da gravação
+write('gravação.wav' , fs , record_voice)
+print('Gravação finalizada!')
